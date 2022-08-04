930b300cf425da7a95708c15594f4561a927d01c
Barry Barron <barrybbarron@gmail.com>
2022-08-03 18:41:58 -0700

    Initial Commit

---
+++ Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modifiers
+//
+//  Created by Barry Barron on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modifiers/ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifiersApp.swift
+//  Modifiers
+//
+//  Created by Barry Barron on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
